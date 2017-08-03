61487e1c5b34c1188bb5904d4b7a49a3358d9586
Vasiliy Shubko <flit77@gmail.com>
2017-08-03 18:36:26 +0200

    main page bootstrapped

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="container">
+      <div class="card-columns">
+        <div class="card p-3">
+          <blockquote class="card-blockquote">
+            <footer>
+              <small>
+                <a target="_blank">
+                  Read more on
+                </a>
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+    <div class="text-center">
+      Loading...
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
