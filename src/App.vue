9c9044336d5c6d657494735c87df9b0ddffc407c
Vasiliy Shubko <flit77@gmail.com>
2017-08-04 23:43:05 +0200

    template updated

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,24 @@
   <div id="app">
     <div class="container">
       <div class="card-columns">
-        <div class="card p-3">
+        <div class="card p-3" v-for="post in posts">
           <blockquote class="card-blockquote">
+            <p>
+              {{ post.data.title }}
+            </p>
             <footer>
               <small>
-                <a target="_blank">
-                  Read more on
+                <a target="_blank" :href="post.data.url">
+                  Read more on {{ post.data.domain }}
                 </a>
               </small>
             </footer>
           </blockquote>
         </div>
       </div>
-    </div>
-    <div class="text-center">
-      Loading...
+      <div class="text-center" v-show="postsLoading">
+        Loading...
+      </div>
     </div>
   </div>
 </template>
